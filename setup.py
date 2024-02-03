4b1d3ef060db326102cbdc85cc3c79bb8a9a4fc8
hiraksarkar <hiraksarkar.cs@gmail.com>
2024-02-03 10:45:05 -0500

    pip installable module

---
+++ setup.py
@@ -0,0 +1,52 @@
+"""
+"""
+import setuptools
+
+setuptools.setup(
+    name='copulacci',
+    version='v0.0.1',
+    python_requires='>=3.9',
+    packages=['copulacci'],
+    package_dir={'': 'src'},
+    author='Hirak Sarkar',
+    author_email='hirak@princeton.edu',
+    description='A count-based model for delineating cell-cell interactions in spatial transcriptomics data',
+    url='https://github.com/raphael-group/copulacci/',
+    install_requires=[
+        'matplotlib',
+        'seaborn',
+        'numpy',
+        'pandas',
+        'seaborn',
+        'scikit-learn',
+        'tqdm',
+        'scipy',
+        'networkx',
+        'jupyterlab',
+        'joblib',
+        'scanpy',
+        'anndata',
+        'omnipath',
+        'squidpy',
+        'spatialdm'
+    ],
+    include_package_data = True,
+    package_data = {
+        '' : ['*.txt']
+        },
+    license='BSD',
+    platforms=["Linux", "MacOs"],
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    keywords=[
+        'spatial transcriptomics',
+        'neural network',
+        'tissue layer'],
+    entry_points={'console_scripts': 'copulacci=copulacci.__main__:main'}
+)
---
+++ src/copulacci/__main__.py
@@ -0,0 +1,19 @@
+"""
+"""
+
+import os,sys
+from copulacci import cci
+from copulacci import spatial
+from copulacci import model
+from copulacci import model2
+
+
+def main():
+    print('Loading internal modules', flush=True)
+    print("Loading spatial data",flush=True)
+
+    ## TODO: Add code here
+
+
+if __name__ == '__main__':
+    main()
